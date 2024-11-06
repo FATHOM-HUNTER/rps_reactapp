5f86083e4408187b85b152e1d6df879959a41c38
Govinda Giri <88205680+FATHOM-HUNTER@users.noreply.github.com>
2024-11-06 13:21:55 +0530

    Update RpsGame.js

--- src/component/RpsGame/RpsGame.js
+++ src/component/RpsGame/RpsGame.js
@@ -9,8 +9,61 @@ const RpsGame = () => {
   const [compScore, setCompScore] = useState(0);
   const [msg, setMsg] = useState("Play Your Move");
   const [compResponse, setCompResponse] = useState(null);
-  const [isDraw, setIsDraw] = useState(false)
+  const [isDraw, setIsDraw] = useState(false);
   const msgRef = useRef(null);
+
+  const genCompChoice = useCallback(() => {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+  }, []);
+
+  const drawGame = () => {
+    setIsDraw(true);
+    setMsg("Game was Draw. Play again.");
+    msgRef.current.style.backgroundColor = "#081b31";
+  };
+
+  const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+      setUserScore((prevScore) => prevScore + 1);
+      setMsg(`You win! Your ${userChoice} beats ${compChoice}`);
+      if (msgRef) {
+        msgRef.current.style.backgroundColor = "green";
+      }
+    } else {
+      setCompScore((prevScore) => prevScore + 1);
+      setMsg(`You lost. ${compChoice} beats your ${userChoice}`);
+      if (msgRef) {
+        msgRef.current.style.backgroundColor = "red";
+      }
+    }
+  };
+
+  const playGame = useCallback(
+    (userChoice) => {
+      const compChoice = genCompChoice();
+      if (compChoice === "Rock") setCompResponse(rockImage);
+      else if (compChoice === "Paper") setCompResponse(paperImage);
+      else setCompResponse(scissorImage);
+
+      if (userChoice === compChoice) {
+        drawGame();
+      } else {
+        setIsDraw(false);
+        let userWin = true;
+        if (userChoice === "Rock") {
+          userWin = compChoice === "Paper" ? false : true;
+        } else if (userChoice === "Paper") {
+          userWin = compChoice === "Scissors" ? false : true;
+        } else {
+          userWin = compChoice === "Rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+      }
+    },
+    [genCompChoice]
+  );
 
   useEffect(() => {
     const choices = document.querySelectorAll(".choice");
@@ -22,83 +75,14 @@ const RpsGame = () => {
 
     choices.forEach((choice) => {
       choice.addEventListener("click", handleClick);
-
-      return () => {
-        choice.removeEventListener("click", handleClick);
-      };
     });
 
-    // Clean up event listeners when component unmounts
     return () => {
       choices.forEach((choice) => {
         choice.removeEventListener("click", handleClick);
       });
     };
-  }, []);
-
-  const genCompChoice = () => {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randIdx = Math.floor(Math.random() * 3);
-    return options[randIdx];
-  };
-
-  const drawGame = () => {
-    //msg
-    setIsDraw(true);
-    setMsg("Game was Draw. Play again.");
-    msgRef.current.style.backgroundColor = "#081b31";
-  };
-
-  const showWinner = (userWin, userChoice, compChoice) => {
-    //userscore, compscore, userscorePara, compscorePara
-    if (userWin) {
-      // userScore++;
-      setUserScore((prevScore) => prevScore + 1);
-      // userScorePara.innerText = userScore;
-      // msg.innerText = `You win! Your ${userChoice} beats ${compChoice}`;
-      setMsg(`You win! Your ${userChoice} beats ${compChoice}`);
-      if (msgRef) {
-        msgRef.current.style.backgroundColor = "green";
-      }
-      //msg.style.backgroundColor = "green";
-    } else {
-      //compScore++;
-      setCompScore((prevScore) => prevScore + 1);
-      // compScorePara.innerText = compScore;
-      setMsg(`You lost. ${compChoice} beats your ${userChoice}`);
-      if (msgRef) {
-        msgRef.current.style.backgroundColor = "red";
-      }
-      //msg.style.backgroundColor = "red";
-    }
-  };
-
-  const playGame = useCallback((userChoice) => {
-    //Generate computer choice
-    const compChoice = genCompChoice();
-    if (compChoice === "Rock") setCompResponse(rockImage);
-    else if (compChoice === "Paper") setCompResponse(paperImage);
-    else setCompResponse(scissorImage);
-
-    if (userChoice === compChoice) {
-      //Draw Game
-      drawGame();
-    } else {
-      setIsDraw(false);
-      let userWin = true;
-      if (userChoice === "Rock") {
-        //scissors, paper
-        userWin = compChoice === "Paper" ? false : true;
-      } else if (userChoice === "Paper") {
-        //rock, scissors
-        userWin = compChoice === "Scissors" ? false : true;
-      } else {
-        //rock, paper
-        userWin = compChoice === "Rock" ? false : true;
-      }
-      showWinner(userWin, userChoice, compChoice);
-    }
-  },[genCompChoice]);
+  }, [playGame]);
 
   return (
     <>
@@ -145,14 +129,12 @@ const RpsGame = () => {
 
       {(userScore > 0 || compScore > 0 || isDraw) && (
         <div>
-          <p id="compRes">Computer's Reponse</p>
+          <p id="compRes">Computer's Response</p>
           <div id="comp-response">
-            <img src={compResponse} alt="computer-reponse" />
+            <img src={compResponse} alt="computer-response" />
           </div>
         </div>
       )}
-
-      {/* <script src="script.js"></script> */}
     </>
   );
 };
